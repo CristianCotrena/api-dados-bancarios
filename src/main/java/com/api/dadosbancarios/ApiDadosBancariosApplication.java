fc8f80b34952445d5fc0ea6b3648612248dcfe2b
Gabrielly Venturini <134551503+GabbyVenturini@users.noreply.github.com>
2023-09-13 11:52:21 -0300

    Iniciado api de dados-bancarios

---
+++ src/main/java/com/api/dadosbancarios/ApiDadosBancariosApplication.java
@@ -0,0 +1,14 @@
+package com.api.dadosbancarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDadosBancariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDadosBancariosApplication.class, args);
+		System.out.println("Hello Word!");
+	}
+
+}
