c09a7a7bf9afd239630e1688d4c67f61a4af2709
Gabrielly Venturini <134551503+GabbyVenturini@users.noreply.github.com>
2023-09-13 12:10:09 -0300

    adicionado hello word a api dados-bancarios

--- src/main/java/com/api/dadosbancarios/ApiDadosBancariosApplication.java
+++ src/main/java/com/api/dadosbancarios/ApiDadosBancariosApplication.java
@@ -2,13 +2,19 @@ package com.api.dadosbancarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiDadosBancariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDadosBancariosApplication.class, args);
-		System.out.println("Hello Word!");
 	}
 
+	@GetMapping("/helloword")
+	public String helloWord(){
+		return "Hello word da api-dados-bancarios";
+	}
 }
